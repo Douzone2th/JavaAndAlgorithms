48383c5560c4997fd96981c6f0a1f8960a370032
Quence1151 <dlgksdyd1151@naver.com>
2022-03-16 14:43:37 +0900

    feat : 3/16, 백준 알고리즘 : [1620번 나는야 포켓몬 마스터 이다솜]

---
+++ hanyong/src/week_8/bj1620.java
@@ -0,0 +1,41 @@
+package week_8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class bj1620 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        HashMap<Integer, String> mapN = new HashMap<>();
+        HashMap<String, Integer> mapS = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < num + 1; i++) {
+            String pmn = br.readLine();
+            mapN.put(i, pmn);
+            mapS.put(pmn, i);
+        }
+        for (int i = 0; i < Q; i++) {
+            String quiz = br.readLine();
+            if(isNumber(quiz)){
+                System.out.println(mapN.get(Integer.parseInt(quiz)));
+            }else{
+                System.out.println(mapS.get(quiz));
+            }
+        }
+    }
+    public static boolean isNumber(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
