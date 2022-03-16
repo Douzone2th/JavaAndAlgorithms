9f8585012fda1cc0692916ba37661bac64c5be81
Lmitiz <ll55kk678@gmail.com>
2022-03-16 13:59:23 +0900

    docs : 파일 구조 변경

--- jiyoung/codingTest/src/map_set/BOJ_1620.java
+++ jiyoung/codingTest/src/map_set/BOJ_1620.java
@@ -1,4 +1,4 @@
-package map_set
+package map_set;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 
 public class BOJ_1620 {
     public static void main(String[] args) throws IOException{
-        //입력받기
+        //�엯�젰諛쏄린
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
@@ -16,8 +16,8 @@ public class BOJ_1620 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        HashMap<String, Integer> name = new HashMap<>(); //이름으로 번호 찾기
-        String num[] = new String[n+1]; //번호로 이름 찾기
+        HashMap<String, Integer> name = new HashMap<>(); //�씠由꾩쑝濡� 踰덊샇 李얘린
+        String num[] = new String[n+1]; //踰덊샇濡� �씠由� 李얘린
         String answer[] = new String[m];
 
         for(int i=1; i<=n; i++) {
@@ -26,14 +26,14 @@ public class BOJ_1620 {
             num[i] = tmp;
         }
 
-        //정답 맞추기
+        //�젙�떟 留욎텛湲�
         for(int i=0; i<m; i++) {
             String tmp = br.readLine();
 
-            if(tmp.charAt(0) < 'A') //숫자 입력받음
+            if(tmp.charAt(0) < 'A') //�닽�옄 �엯�젰諛쏆쓬
                 sb.append(num[Integer.parseInt(tmp)]+"\n");
             
-            else // 문자 입력받음
+            else // 臾몄옄 �엯�젰諛쏆쓬
                 sb.append(name.get(tmp)+"\n");
         }
         System.out.println(sb);
