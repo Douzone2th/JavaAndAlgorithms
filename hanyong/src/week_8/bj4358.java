c06742224c900103111cbe18e8d069ffd24c9c12
Quence1151 <dlgksdyd1151@naver.com>
2022-03-20 13:11:40 +0900

    feat : 3/18, 백준 알고리즘 : [4358번 생태학]

---
+++ hanyong/src/week_8/bj4358.java
@@ -0,0 +1,35 @@
+package week_8;
+
+import java.io.*;
+import java.util.*;
+
+public class bj4358 {
+    static HashMap<String, Integer> trees;
+    static int total;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        trees = new HashMap<String, Integer>();
+        String str = br.readLine();
+
+        do {
+            trees.put(str, trees.getOrDefault(str, 0) + 1);
+            total++;
+            str = br.readLine();
+        } while (str == null || str.length() == 0);
+
+        Object[] keys = trees.keySet().toArray();
+        Arrays.sort(keys);
+
+        StringBuilder sb = new StringBuilder();
+        for (Object key : keys) {
+            String keyStr = (String) key;
+            int count = trees.get(keyStr);
+            double per = (double) (count * 100.0) / count;
+
+            sb.append(keyStr + " " + String.format("%.4f", per) + "\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
