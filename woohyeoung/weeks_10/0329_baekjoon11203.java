cc2f622bf39a7f4543a4adcea5b3cd11bc784ced
woohyeoung <dngud951108@gmail.com>
2022-03-30 00:10:27 +0900

    feat : 3/29, 백준알고리즘 : [11203번 Numbers On a Tree]

---
+++ woohyeoung/weeks_10/0329_baekjoon11203.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		long testcase = 0;
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			String[] strAry = br.readLine().split(" ");
+			if(strAry.length > 1) {
+				testcase = Integer.parseInt(strAry[0]);
+				testcase = solution(testcase, strAry[1]);
+			}else {
+				testcase = (long)Math.pow(2, Integer.parseInt(strAry[0])+1)-1;
+			}
+			System.out.println(testcase);
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		}
+	}
+	public static long solution(long testcase, String str) {
+		long num = (long)Math.pow(2, testcase +1);
+		int k = 1;
+		String[] strAry = str.split("");
+		for(int i = 0; i < strAry.length; i++) {
+			if(strAry[i].equals("L")) k *= 2;
+			else k = (k*2) +1;
+		}
+		return num - k;
+	}
+}
+
+
