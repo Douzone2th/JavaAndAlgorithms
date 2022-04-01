6f51e2564775930bb15bd348edfb3f1a1151f94e
236 <sks1tp@naver.com>
2022-04-01 16:37:48 +0900

    feat : 3/31, 백준 알고리즘 : [15650번 N과M(2)]

--- sangyoon/Baekjoon/src/week_10/Baekjoon_15650.java
+++ sangyoon/Baekjoon/src/week_10/Baekjoon_15650.java
@@ -1,33 +1,47 @@
 package week_10;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Baekjoon_15650 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-//		int a;
-//		
-//		for (int i = 1; i <= N; i++) {
-//			a = 1;
-//			while(a < N) {
-//				
-//			}
-//		}
-		
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				System.out.println(i + " " + j);
-			}
-		}
-		
-		
-		
-	}
+public class Baekjoon_15650 { 
+	static int M, N; 
+	static int[] arr; 
+	static boolean[] visit; 
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	static int index = 1; 
+	public static void main(String[] args) throws IOException { 
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(bf.readLine()); 
+		N = Integer.parseInt(st.nextToken()); 
+		M = Integer.parseInt(st.nextToken()); 
+		arr = new int[M]; visit = new boolean[N + 1];
+		recursion(0); 
+		bw.flush(); 
+		bw.close(); 
+	} 
+	public static void recursion(int index) throws IOException { 
+		if (index == M) { 
+			for (int i = 0; i < M; i++) { 
+				bw.write(arr[i] + " "); 
+				} 
+			bw.write("\n");
+			return; 
+			} 
+			for (int i = 1; i <= N; i++) {
+				if(!visit[i]) { 
+					arr[index] = i;
+					visit[i] = true;
+					recursion(index + 1);
+					for (int j = i + 1; j <= N; j++) {
+						visit[j] = false; 
+					} 
+				} 
+			} 
+	} 
 }
+
+
