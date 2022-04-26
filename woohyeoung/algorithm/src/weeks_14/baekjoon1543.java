074778ebc574b827ba56c7f13ece2ae51b401a4c
woohyeoung <dngud951108@gmail.com>
2022-04-26 20:03:07 +0900

    feat : 4/26, 백준 알고리즘 : [1543번 문서검색]

---
+++ woohyeoung/algorithm/src/weeks_14/baekjoon1543.java
@@ -0,0 +1,28 @@
+package weeks_14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon1543 {
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			String testcase = br.readLine();
+			String ans = br.readLine();
+			System.out.println(solution(testcase, ans));
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		}
+	}
+	public static int solution(String testcase, String ans) {
+		int count = 0;
+		testcase = testcase.replaceAll(String.format("(%s)", ans), "1");
+		char[] ch = testcase.toCharArray();
+		for(int i = 0; i < testcase.length(); i++) {
+			if(ch[i] == '1') count ++;
+		}
+		return count;
+	}
+}
