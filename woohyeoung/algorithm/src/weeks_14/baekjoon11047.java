be3515a77de3657e329e96352537d60f20b572d5
woohyeoung <dngud951108@gmail.com>
2022-04-28 09:59:08 +0900

    feat : 4/28, 백준 알고리즘 : [11047번 동전 0]

---
+++ woohyeoung/algorithm/src/weeks_14/baekjoon11047.java
@@ -0,0 +1,38 @@
+package weeks_14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon11047 {
+	static int[] ary;
+	static int count = 0;
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		StringTokenizer stz;
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			stz = new StringTokenizer(br.readLine());
+			int testcase = Integer.parseInt(stz.nextToken());
+			int value = Integer.parseInt(stz.nextToken());
+			ary = new int[testcase];
+			for(int i = 0; i < testcase; i++) {
+				ary[i] = Integer.parseInt(br.readLine());
+			}
+			solution(value, testcase);
+			System.out.println(count);
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		}
+	}
+	public static void solution(int value, int n) {
+		for (int i = n-1; i >= 0 ; i--) {
+			if(ary[i] <= value) {
+				count += value / ary[i];
+				value %= ary[i];
+				
+			}
+		}
+	}
+}
