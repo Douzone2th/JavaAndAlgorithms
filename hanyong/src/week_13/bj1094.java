9a5ddf7a6220e1f0007987a50ab3ef43771aa93f
Quence1151 <dlgksdyd1151@naver.com>
2022-05-02 22:59:06 +0900

    feat : 5/2, 백준 알고리즘 : [1913 달팽이]

--- hanyong/src/week_13/bj1094.java
+++ hanyong/src/week_13/bj1094.java
@@ -1,22 +1,22 @@
 package week_13;
 
-import java.io.*;
+    import java.io.*;
 
-public class bj1094 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int staff = 64;
-        int count = 0;
-        int length = Integer.parseInt(br.readLine());
-        while (length > 0) {
-            if (staff > length) {
-                staff /= 2;
+    public class bj1094 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int staff = 64;
+            int count = 0;
+            int length = Integer.parseInt(br.readLine());
+            while (length > 0) {
+                if (staff > length) {
+                    staff /= 2;
 
-            } else {
-                length -= staff;
-                count++;
+                } else {
+                    length -= staff;
+                    count++;
+                }
             }
+            System.out.println(count);
         }
-        System.out.println(count);
     }
-}
